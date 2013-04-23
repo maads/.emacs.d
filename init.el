1fa518f79bda32401f2888e9b399876c74a0dfac
Mads Tordal <mads.tordal@gmail.com>
2013-04-23 14:16:12 +0200

    Added paredit

--- init.el
+++ init.el
@@ -145,3 +145,17 @@
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 ;; MS Windows clipboard is UTF-16LE 
 (set-clipboard-coding-system 'utf-16le-dos)
+
+
+
+;; ParEdit
+(require 'paredit)
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
