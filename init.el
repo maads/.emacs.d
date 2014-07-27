584a2c2d6d751f443e720a13a033c0c2dab42ac5
Mads Tordal <mads.tordal@sysco.no>
2014-07-27 18:06:52 -0500

    add line numbers by default, and comment out paredit

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(global-linum-mode nil) ;; linjenummer
+ '(global-linum-mode t) ;; linjenummer
  '(scroll-bar-mode nil)
  '(tool-bar-mode)
  '(menu-bar-mode))
@@ -149,13 +149,13 @@
 
 
 ;; ParEdit
-(require 'paredit)
+;;(require 'paredit)
 
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+;;(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+;;(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+;;(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+;;(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+;;(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+;;(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;;(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
